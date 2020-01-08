ae3840c24a74e2011c8e2aa255964d6e49a9186c
StuartSul <ssstuartss@gmail.com>
2020-01-09 00:51:23 +0900

    Updates

---
+++ precache-manifest.f58b0c61190b185cbd11f7dbcb35b02b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0ccc9ac04eb72a0ce63d904161173b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34f8b7d1375474e3569f",
+    "url": "/static/css/main.66eab46e.chunk.css"
+  },
+  {
+    "revision": "70f05c0cb037fb4263cb",
+    "url": "/static/js/2.4f00cd78.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4f00cd78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34f8b7d1375474e3569f",
+    "url": "/static/js/main.12827cd9.chunk.js"
+  },
+  {
+    "revision": "7c1dd0200c68698bb36e",
+    "url": "/static/js/runtime-main.e47c3bb0.js"
+  }
+]);
